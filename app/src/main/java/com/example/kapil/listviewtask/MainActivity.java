6ddbc29dfa8e81edba3c6a0f109c83fcca2207c3
Kapil Thakur <kapil886060@gmail.com>
2018-07-29 11:01:49 +0530

    Completed
    Commit

---
+++ app/src/main/java/com/example/kapil/listviewtask/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.kapil.listviewtask;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ExpandableListView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    HashMap<String, List<String>> Movies_category;
+    List<String> Movies_list;
+    ExpandableListView Exp_list;
+    MoviesAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Exp_list = (ExpandableListView) findViewById(R.id.exp_list);
+        Movies_category = DataProvider.getInfo();
+        Movies_list = new ArrayList<String>(Movies_category.keySet());
+        adapter = new MoviesAdapter(this, Movies_category, Movies_list);
+        Exp_list.setAdapter(adapter);
+
+    }
+}
